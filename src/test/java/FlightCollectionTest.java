2a27df936211dfe405149bdb04a52e58acf9c571
ylii0681 <ylii0681@student.monash.edu>
2025-03-24 00:19:26 +1100

    add one test case testing right flight ID FlightCollectionTest.java

--- src/test/java/FlightCollectionTest.java
+++ src/test/java/FlightCollectionTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -22,6 +23,7 @@ public class FlightCollectionTest {
     @BeforeEach
     public void setup(){
         flightCollection = new FlightCollection();
+        FlightCollection.flights = new ArrayList<>();
     }
 
     @Test
@@ -33,18 +35,28 @@ public class FlightCollectionTest {
         Timestamp timestampTo = Timestamp.valueOf(dateTo);
         Airplane airplane = new Airplane(1, "A", 1,2,3);
         Flight flight = new Flight(123, "Melbourne", "Sydney", "MS1234", "JetStar", timestampFrom, timestampTo, airplane);
+        FlightCollection.flights.add(flight);
         assertEquals(null, flightCollection.getFlightInfoById(321));
     }
 
     @Test
-    @DisplayName("validate if the flight is null")
-    public void testGetFlightInfoByIDWithFlightValueIsNull(){
-        Flight flight = new Flight();
-        flight = null;
+    @DisplayName("validate if the flights are null")
+    public void testGetFlightInfoByIDWithFlightsValueAreNull(){
         assertEquals(null, flightCollection.getFlightInfoById(123));
     }
 
-
+    @Test
+    @DisplayName("validate if the flight ID is matched")
+    public void testGetFlightInfoByIDWithMachedID(){
+        LocalDateTime dateFrom = LocalDateTime.of(2025, 3, 22, 11, 2, 0, 0);
+        LocalDateTime dateTo = LocalDateTime.of(2025, 3, 22, 12, 1, 0, 0);
+        Timestamp timestampFrom = Timestamp.valueOf(dateFrom);
+        Timestamp timestampTo = Timestamp.valueOf(dateTo);
+        Airplane airplane = new Airplane(1, "A", 1,2,3);
+        Flight flight = new Flight(123, "Melbourne", "Sydney", "MS1234", "JetStar", timestampFrom, timestampTo, airplane);
+        FlightCollection.flights.add(flight);
+        assertEquals(flight, flightCollection.getFlightInfoById(123));
+    }
 
 
 }
